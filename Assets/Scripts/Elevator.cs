552454cf43f598a0f3a63424d2c9be29e553fe48
Areyouthere003 <jgerazo7@misena.edu.co>
2024-09-02 13:55:42 -0500

    added new prefabs from pot, cover pot, fixed the interaction buttons with the hand

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -4,46 +4,76 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
+    [SerializeField] float speed = 0.5f;
     [SerializeField] GameObject platform;
     [SerializeField] Transform startPoint;
     [SerializeField] Transform finalPoint;
     
     Vector3 moveTo;
     bool valid = false;
-    
+    float valid2 = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (valid)
-            platform.transform.position = Vector3.MoveTowards(platform.transform.position, moveTo, speed * Time.deltaTime);
-
-        if (platform.transform.position == startPoint.position)
+        if (valid) //up & down
         {
-            StartCoroutine(TiempoEspera());
-            moveTo = finalPoint.position;
-            valid = false;
-
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, moveTo, speed * Time.deltaTime);
+            
         }
 
-        if (platform.transform.position == finalPoint.position)
+        if(platform.transform.position == finalPoint.transform.position)
         {
-            StartCoroutine(TiempoEspera());
-            moveTo = startPoint.position;
             valid = false;
+        }
+
+        if (platform.transform.position == startPoint.transform.position)
+        {
+            valid = false;
+        }
+
+    }
+    
+    public void GetUpDown()
+    {
+        switch(valid2)
+        {
+            case 0:
+                moveTo = finalPoint.position;
+                valid = true;
+                valid2++;
+                break;
+            case 1:
+                moveTo = startPoint.position;
+                valid = true;
+                valid2--;
+                break;
+            default:
+                valid = false;
+                valid2 = 0;
+                break;
         }
     }
-    IEnumerator TiempoEspera()
+
+    private void OnTriggerEnter(Collider other)
     {
-        valid = false;
-        yield return new WaitForSeconds(5);
-        valid = true;
+        if(other.transform.CompareTag("Player"))
+        {
+            other.transform.parent = platform.transform;
+         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
     }
 }
 
